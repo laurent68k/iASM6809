9355f05d468032a6d032b27bbcaac746b8fa9440
Laurent <laurent@caprica.local>
2016-11-02 00:02:16 +0100

    Initial Commit

---
+++ i6809asm/as9n.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+#define strncasecmp strncmp
+
+#include "as.h"
+#include "table9.h"
+
+
+#undef DEBUG
+
+#include "as.c"
+#include "do9.c"
+#include "pseudo.c"
+#include "eval.c"
+#include "symtab.c"
+#include "util.c"
+#include "ffwd.c"
+#include "output.c"
---
+++ i6809asm/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  i6809asm
+//
+//  Created by Laurent on 03/11/12.
+//  Copyright (c) 2012 Laurent. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    
+    return main6809(argc, argv);
+}
+
